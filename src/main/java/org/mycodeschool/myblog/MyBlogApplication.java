db147066f0046588b1b8dc61ee81a1e2f06d2a53
2z75 <zinedine.chader@gmail.com>
2024-07-01 12:02:16 +0200

    test commit

---
+++ src/main/java/org/mycodeschool/myblog/MyBlogApplication.java
@@ -0,0 +1,16 @@
+package org.mycodeschool.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class MyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogApplication.class, args);
+        System.out.println("Hello World");
+	}
+
+}
